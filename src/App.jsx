5b60e22a5ad1bbabd40f7d281921858a0605008f
PalstarKe <maishwriter2@gmail.com>
2023-12-07 00:09:40 +0300

    edited services section

--- src/App.jsx
+++ src/App.jsx
@@ -9,14 +9,14 @@ const App = () => (
     <section className="xl:padding-l wide:padding-r padding-b">
       <Hero/>
     </section>
+    <section className="padding-x py-10">
+      <Services/>
+    </section>
     <section className="padding">
       <PopularProducts/>
     </section>
     <section className="padding">
       <SuperQuality/>
-    </section>
-    <section className="padding-x py-10">
-      <Services/>
     </section>
     <section className="padding">
       <SpecialOffers/>
